112d2be43953421bfeb196b26c5707f38223b8fe
G-rm4n <oligah06@gmail.com>
2026-02-04 22:39:03 -0300

    add translator test

--- Tests/TestTranslator.py
+++ Tests/TestTranslator.py
@@ -1,5 +1,5 @@
 from Adapters.Translator import Translator
 
-print("movimient not translated: (1125899906842624, 17179869184, 'P')")
+print("movimient not translated: (1125899906842624, 2251799813685248, 'K')")
 
-print(f"movimiento translated: {Translator.translateMove((2251799813685248, 288230376151711744, 'K'))}")
+print(f"movimiento translated: {Translator.translateMove((1125899906842624, 2251799813685248, 'K'))}")

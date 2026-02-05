388cb56a746a29a51f64a2f1ff8db4b266fd1f59
G-rm4n <oligah06@gmail.com>
2026-02-04 22:22:43 -0300

    fix: check detection bug

--- Bot_Engine/legalCheck.py
+++ Bot_Engine/legalCheck.py
@@ -32,6 +32,10 @@ class legalChecker:
         enemyBishops=Bitboards[10 if color=="W" else 3]
         enemyKing=Bitboards[7 if color=="W" else 0]
 
+        possiblePawns=((((kingBitboard & (~ROW_8) & (~COL_8))<<9)   | 
+                        ((kingBitboard & (~ROW_8) & (~COL_1))<<7)   |
+                        (((kingBitboard & (~ROW_1) & (~COL_1)))>>9) |
+                        ((kingBitboard) & (~ROW_1) & (~COL_8))>>7))
 
         PossibleHorses=((((kingBitboard & (~(COL_8)) & (~(ROW_8_7)))<<17)) |
                          ((kingBitboard & (~(COL_1)) & (~(ROW_8_7)))<<15) | 
@@ -42,39 +46,54 @@ class legalChecker:
                          ((kingBitboard & (~(COL_1_2)) & (~(ROW_1)))>>10) | 
                          ((kingBitboard & (~(COL_8_7)) & (~(ROW_1)))>>6))
         
+        possibleKingPosition=(((kingBitboard & (~(ROW_8)))<<8)   |
+                            ((kingBitboard & (~(ROW_1)))>>8) | 
+                            ((kingBitboard & (~(COL_8)))<<1) | 
+                            ((kingBitboard & (~(COL_1)))>>1) |
+                            ((kingBitboard & (~(ROW_8)) & (~(COL_8)))<<9) |
+                            ((kingBitboard & (~(ROW_8)) & (~(COL_1)))<<7) |
+                            ((kingBitboard & (~(ROW_1)) & (~(COL_1)))>>9) |
+                            ((kingBitboard & (~(ROW_1)) & (~(COL_8)))>>7))
+        
         if PossibleHorses & enemyHorses:
+            return False
+
+        if possibleKingPosition & enemyKing:
+            return False
+
+        if possiblePawns & enemyPawns:
             return False
         
         Vray1=RayGenerator.GenerateVerticalRay(Bitboards,kingBitboard,dy=1)
         Vray2=RayGenerator.GenerateVerticalRay(Bitboards,kingBitboard,dy=(-1))
         
-        if (Vray1 | Vray2) & (enemyQueen | enemyTowers | enemyKing):
+        if (Vray1 | Vray2) & (enemyQueen | enemyTowers):
             return False
         
         Hray1=RayGenerator.GenerateHorizontalRay(Bitboards,kingBitboard,dx=1)
         Hray2=RayGenerator.GenerateHorizontalRay(Bitboards,kingBitboard,dx=(-1))
 
-        if (Hray1 | Hray2) & (enemyQueen | enemyTowers | enemyKing):
+        if (Hray1 | Hray2) & (enemyQueen | enemyTowers):
             return False
         
         diagRay1=RayGenerator.GenerateDiagonalRay(Bitboards,kingBitboard,dx=1,dy=1)
 
-        if diagRay1 &  (enemyPawns | enemyQueen | enemyBishops | enemyKing):
+        if diagRay1 &  (enemyQueen | enemyBishops):
             return False
         
         diagRay2=RayGenerator.GenerateDiagonalRay(Bitboards,kingBitboard,dx=1,dy=(-1))
 
-        if diagRay2 &  (enemyPawns | enemyQueen | enemyBishops | enemyKing):
+        if diagRay2 &  (enemyQueen | enemyBishops):
             return False        
         
         diagRay3=RayGenerator.GenerateDiagonalRay(Bitboards,kingBitboard,dx=(-1),dy=1)
 
-        if diagRay3 &  (enemyPawns | enemyQueen | enemyBishops | enemyKing):
+        if diagRay3 &  (enemyQueen | enemyBishops):
             return False
         
         diagRay4=RayGenerator.GenerateDiagonalRay(Bitboards,kingBitboard,dx=(-1),dy=(-1))
 
-        if diagRay4 &  (enemyPawns | enemyQueen | enemyBishops | enemyKing):
+        if diagRay4 &  (enemyQueen | enemyBishops):
             return False
 
 
